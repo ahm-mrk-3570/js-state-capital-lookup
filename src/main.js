1b0fff05f90a606cb2b402394fb0a64d5fcdf971
AHM MRK <ahmmrk.3570m@gmail.com>
2026-05-31 15:07:49 +0330

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
 const searchList = async (value) => {
-  const res = await fetch("/data/states.json");
+  const res = await fetch(`${import.meta.env.BASE_URL}/data/states.json`);
   const states = await res.json();
 
   let matchesStates = states.filter((state) => {
